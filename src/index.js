b41cbf30a4739c7e696eb70d58c122f5bf6934b1
Philip <fabianekphilip@gmail.com>
2021-12-14 21:25:04 +0100

    Adjust 'react' and 'react-dom' versions, adjust render method indide index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 
 import './index.css';
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
-root.render(<App />);
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
